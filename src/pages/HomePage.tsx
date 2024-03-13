67a5d080a4fa71c92b4157e7c825b0d2d7cf014f
Evgeniy <kostyrkoevgeniy@yandex.ru>
2024-03-13 22:43:34 +0300

    caltTotalPrice, increment & decrement quantity, delete cart

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,34 +1,27 @@
-import { Col, List, Row } from "antd";
+import { Col, Row } from "antd";
 import Title from "antd/es/typography/Title";
-import { useAppDispatch, useAppSelector } from "../features/store";
-import { cartSelect } from "../features/slice/cartSelector";
-import { useEffect } from "react";
-import { getCart } from "../features/services/cartApi";
+import Cart from "../components/Cart";
+import CartCost from "../components/CartCost";
 
 const HomePage = () => {
-  const dispatch = useAppDispatch();
-  const cart = useAppSelector(cartSelect);
-
-  useEffect(() => {
-    dispatch(getCart());
-  }, [dispatch]);
-
   return (
     <Row>
       <Col flex={3}>
-        <Title>Список продуктов</Title>
-        <List>
-          {cart ? (
-            cart.map((prod) => {
-              return <Title key={prod.id}>{prod.title}</Title>;
-            })
-          ) : (
-            <></>
-          )}
-        </List>
+        <Title style={{ display: "flex", justifyContent: "center" }}>
+          Список продуктов
+        </Title>
+        <Cart />
       </Col>
-      <Col flex={1}>
+      <Col
+        flex={1}
+        style={{
+          display: "flex",
+          justifyContent: "start",
+          flexDirection: "column",
+        }}
+      >
         <Title>Корзина</Title>
+        <CartCost />
       </Col>
     </Row>
   );
